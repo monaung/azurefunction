27318717b0a6b91981d7678c18601d5eb2ae8b3a
Wynn @ Mon Aung <mon.aung@gmail.com>
2024-10-22 17:55:29 +1300

    function created

---
+++ MyFirstFunction.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace MonAung.Function
+{
+    public class MyFirstFunction
+    {
+        private readonly ILogger<MyFirstFunction> _logger;
+
+        public MyFirstFunction(ILogger<MyFirstFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("MyFirstFunction")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
